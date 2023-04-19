579f244f8b92e1188eb0f3721aed0f296db65d35
CraigMcGuirk <craigmcguirkw@gmail.com>
2023-04-19 21:25:25 +0000

    add deposit function

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def deposit():
+    while True:
+        amount = input("How much would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be grater than 0.")
+        else:
+            print("You must input a number")
+
+    return amount 
